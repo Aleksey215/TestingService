9649e6314b2453b15a351358ac246532feb4bcd5
Aleksey215 <kaloha215@gmail.com>
2022-12-04 22:03:31 +1000

    set up database (PostgreSQL)

--- Testing/config/settings.py
+++ Testing/config/settings.py
@@ -76,11 +76,22 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'testing_db',
+        'USER': 'testing_user',
+        'PASSWORD': 'testing1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    },
 }
 
 
